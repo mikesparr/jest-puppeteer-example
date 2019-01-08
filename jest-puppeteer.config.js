ac989da264c7683d0806921dfd287dbb0916574d
Mike Sparr <msparr@pointsixsolutions.com>
2019-01-08 11:29:44 -0700

    Fix spacing that was bugging me ;-)

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,7 @@ module.exports = {
         dumpio: false,
         headless: process.env.HEADLESS !== 'false',   // export HEADLESS=false to launch Chromium
         devtools: true,                               // optionally display devtools in non-headless mode
-        slowMo: 100, 				                  // optionally slow down typing
+        slowMo: 100,                                  // optionally slow down typing
         defaultViewport: {                            // override default 800x600 pixel browser setting
             width: 1024,
             height: 768
